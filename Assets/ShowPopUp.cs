f52404bb5d80a6daef75caf7d4c6fb29c572ac7d
Jaanek Kerma <jaanek.kerma@gmail.com>
2023-09-18 08:33:12 +0200

    Prefabs + Level 0
    -0. Reorginized many prefabs
    -1. Movement Tutorial Basic

---
+++ Assets/ShowPopUp.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ShowPopUp : MonoBehaviour
+{
+    [SerializeField] private GameObject PopupToShow;
+    [TextArea(10, 10)][SerializeField] private string PopupText;
+    [SerializeField] private AudioClip[] ShowSound, HideSound;
+
+    private AudioSource Audio_Source;
+    private TextMeshProUGUI Popup_Text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Audio_Source = GetComponent<AudioSource>();
+        Popup_Text = PopupToShow.GetComponentInChildren<TextMeshProUGUI>();
+        Popup_Text.text = PopupText;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PopupToShow.SetActive(true);
+        }
+    }
+}
